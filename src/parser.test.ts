78b118b01c35f14fbda23e3cf260052e16af8f2b
awaji-daisuke <awaji_daisuke@intec.co.jp>
2021-07-16 19:53:39 +0900

    fix issue of ref parcer

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -92,4 +92,129 @@ describe("parse", () => {
       },
     });
   });
+
+  test("Replace Valiables: Ref, Sub, FindInMap 2", async () => {
+    const template = await readCfnFile(
+      path.join(__dirname, "../sample/cloudfront.yaml")
+    );
+
+    const parced = parse(template);
+    const parsedStr = JSON.stringify(parced);
+    expect(parsedStr).not.toContain("Ref");
+    expect(parsedStr).not.toContain("Sub");
+    expect(parsedStr).not.toContain("FindInMap");
+    expect(parced).toMatchObject({
+      AWSTemplateFormatVersion: "2010-09-09",
+      Parameters: {
+        Env: {
+          Type: "String",
+          AllowedValues: ["prod", "test"],
+          Default: "test",
+        },
+      },
+      Mappings: {
+        CloudFront: {
+          Alias: {
+            prod: "a.com",
+            test: "b.com",
+          },
+          AcmCertificateArn: {
+            prod: "arn",
+            test: "arn",
+          },
+        },
+      },
+      Resources: {
+        S3Bucket: {
+          Type: "AWS::S3::Bucket",
+          Properties: {
+            AccessControl: "PublicRead",
+            BucketName: "test-bucket",
+            WebsiteConfiguration: {
+              IndexDocument: "spec.html",
+              ErrorDocument: "error.html",
+            },
+          },
+        },
+        BucketPolicy: {
+          Type: "AWS::S3::BucketPolicy",
+          Properties: {
+            Bucket: "DEFAULT",
+            PolicyDocument: {
+              Statement: [
+                {
+                  Action: ["s3:GetObject"],
+                  Effect: "Allow",
+                  Resource: "arn:aws:s3:::S3Bucket/*",
+                  Principal: {
+                    CanonicalUser: {
+                      "Fn::GetAtt": [
+                        "CloudFrontOriginAccessIdentity",
+                        "S3CanonicalUserId",
+                      ],
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+        CloudFrontOriginAccessIdentity: {
+          Type: "AWS::CloudFront::CloudFrontOriginAccessIdentity",
+          Properties: {
+            CloudFrontOriginAccessIdentityConfig: {
+              Comment: "access identity",
+            },
+          },
+        },
+        CloudFrontDistribution: {
+          Type: "AWS::CloudFront::Distribution",
+          DependsOn: ["CloudFrontOriginAccessIdentity"],
+          Properties: {
+            DistributionConfig: {
+              Enabled: true,
+              Aliases: ["b.com"],
+              ViewerCertificate: {
+                AcmCertificateArn: "arn",
+                SslSupportMethod: "sni-only",
+              },
+              DefaultCacheBehavior: {
+                AllowedMethods: ["HEAD", "GET"],
+                CachedMethods: ["HEAD", "GET"],
+                DefaultTTL: 0,
+                MaxTTL: 0,
+                MinTTL: 0,
+                TargetOriginId: "test-bucket-Origin",
+                ViewerProtocolPolicy: "redirect-to-https",
+                ForwardedValues: {
+                  QueryString: false,
+                },
+              },
+              IPV6Enabled: true,
+              HttpVersion: "http2",
+              DefaultRootObject: "spec.html",
+              Origins: [
+                {
+                  Id: "test-bucket-Origin",
+                  DomainName: {
+                    "Fn::GetAtt": ["S3Bucket", "DomainName"],
+                  },
+                  S3OriginConfig: {
+                    OriginAccessIdentity:
+                      "origin-access-identity/cloudfront/CloudFrontOriginAccessIdentity",
+                  },
+                },
+              ],
+              CustomErrorResponses: [
+                {
+                  ErrorCachingMinTTL: 0,
+                  ErrorCode: 403,
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+  });
 });
